72a61697dfa95a15eb76478319eb83681152a4a5
Thomas Schubert <funthomas424242@googlemail.com>
2013-05-14 19:21:59 +0200

    Folder renamed to Verzeichnis

--- Plugins/jOrdner/src/main/java/com/gh/jordner/gui/parts/VerzeichnisListPart.java
+++ Plugins/jOrdner/src/main/java/com/gh/jordner/gui/parts/VerzeichnisListPart.java
@@ -19,7 +19,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
-public class FolderListPart {
+public class VerzeichnisListPart {
 
 	private Label label;
 	private TableViewer tableViewer;
