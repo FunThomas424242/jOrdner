bfd41de8ccebc4899f0d67193effa7c2f1cedbf8
Thomas Schubert <funthomas424242@googlemail.com>
2013-06-02 19:51:06 +0200

    Nullpointer abgefangen und Message ausgegeben

--- Plugins/jOrdner.jpa/src/com/gh/jordner/jpa/VerzeichnisServiceDAOImpl.java
+++ Plugins/jOrdner.jpa/src/com/gh/jordner/jpa/VerzeichnisServiceDAOImpl.java
@@ -12,29 +12,30 @@ import com.gh.jordner.api.Verzeichnis;
 
 @Creatable
 public class VerzeichnisServiceDAOImpl implements VerzeichnisDAO {
-	
-	
-	
+
 	public EntityManagerFactory lookupEMF(String unitName) {
-		BundleContext context=com.gh.jordner.jpa.osgi.Activator.getContext();
-	    ServiceReference[] refs = null;
-	    try {
-	        refs = context.getServiceReferences(
-	                              EntityManagerFactory.class.getName(), 
-	                              "(osgi.unit.name="+unitName+")");
-	    } catch (InvalidSyntaxException isEx) {
-	        throw new RuntimeException("Filter error", isEx);
-	    }
-	    return (refs == null)
-	        ? null
-	        : (EntityManagerFactory) context.getService(refs[0]);
+		BundleContext context = com.gh.jordner.jpa.osgi.Activator.getContext();
+		ServiceReference[] refs = null;
+		try {
+			refs = context.getServiceReferences(
+					EntityManagerFactory.class.getName(), "(osgi.unit.name="
+							+ unitName + ")");
+		} catch (InvalidSyntaxException isEx) {
+			throw new RuntimeException("Filter error", isEx);
+		}
+		return (refs == null) ? null : (EntityManagerFactory) context
+				.getService(refs[0]);
 	}
 
 	public Verzeichnis create(Verzeichnis verzeichnis) {
 		System.out.println(verzeichnis);
-		EntityManagerFactory emf=lookupEMF("Accounts");
-//		EntityManager em = emf.createEntityManager();
-//		em.persist(verzeichnis);
+		EntityManagerFactory emf = lookupEMF("Accounts");
+		if (emf != null) {
+			EntityManager em = emf.createEntityManager();
+			em.persist(verzeichnis);
+		}else{
+			System.out.println("No EntityManagerFactory found");
+		}
 		return verzeichnis;
 	}
 
