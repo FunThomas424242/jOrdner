9c17193a7ae2f27e4d926a0909e690ad3a0bafad
Thomas Schubert <funthomas424242@googlemail.com>
2013-06-11 00:21:15 +0200

    robuster implementiert

--- Plugins/jOrdner.jpa/src/main/java/com/gh/jordner/jpa/VerzeichnisServiceDAOImpl.java
+++ Plugins/jOrdner.jpa/src/main/java/com/gh/jordner/jpa/VerzeichnisServiceDAOImpl.java
@@ -2,10 +2,11 @@ package com.gh.jordner.jpa;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 import org.eclipse.e4.core.di.annotations.Creatable;
 import org.eclipse.gemini.ext.di.GeminiPersistenceContext;
+import org.eclipse.gemini.ext.di.GeminiPersistenceProperty;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 import com.gh.jordner.api.Verzeichnis;
 import com.gh.jordner.jpa.osgi.Activator;
@@ -13,41 +14,107 @@ import com.gh.jordner.jpa.osgi.Activator;
 @Creatable
 public class VerzeichnisServiceDAOImpl implements VerzeichnisDAO {
 
-	
+	// @Inject
+	// @GeminiPersistenceContext(unitName = "Accounts" , properties = {
+	// @GeminiPersistenceProperty(name=JDBC_DRIVER,
+	// value="com.mysql.jdbc.Driver"),
+	// @GeminiPersistenceProperty(name=JDBC_URL,
+	// value="jdbc:mysql://127.0.0.1/contacts"),
+	// @GeminiPersistenceProperty(name=JDBC_USER, value="contact"),
+	// @GeminiPersistenceProperty(name=JDBC_PASSWORD, value="contact"),
+	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_DRIVER,
+	// valuePref = @Preference("jdbc_driver")),
+	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_URL,
+	// valuePref = @Preference("jdbc_url")),
+	// @GeminiPersistenceProperty(name =
+	// PersistenceUnitProperties.LOGGING_LEVEL, value = "FINE"),
+	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING,
+	// value = "false"),
+	// @GeminiPersistenceProperty(name =
+	// PersistenceUnitProperties.WEAVING_INTERNAL, value = "false")
+	// @GeminiPersistenceProperty(name=DDL_GENERATION, value=DROP_AND_CREATE),
+	// @GeminiPersistenceProperty(name=DDL_GENERATION_MODE,
+	// value=DDL_DATABASE_GENERATION) })
+
 	@Inject
-	@GeminiPersistenceContext(unitName = JPAConstants.JPA_UNIT)
-//	@Inject
-//	@GeminiPersistenceContext(unitName = "Accounts" , properties = {
-//	@GeminiPersistenceProperty(name=JDBC_DRIVER, value="com.mysql.jdbc.Driver"),
-//	@GeminiPersistenceProperty(name=JDBC_URL, value="jdbc:mysql://127.0.0.1/contacts"),
-//	@GeminiPersistenceProperty(name=JDBC_USER, value="contact"),
-//	@GeminiPersistenceProperty(name=JDBC_PASSWORD, value="contact"),
-//	@GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_DRIVER, valuePref = @Preference("jdbc_driver")),
-//    @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_URL, valuePref = @Preference("jdbc_url")),
-//    @GeminiPersistenceProperty(name = PersistenceUnitProperties.LOGGING_LEVEL, value = "FINE"),
-//    @GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING, value = "false"),
-//    @GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING_INTERNAL, value = "false")
-//	@GeminiPersistenceProperty(name=DDL_GENERATION, value=DROP_AND_CREATE),
-//	@GeminiPersistenceProperty(name=DDL_GENERATION_MODE, value=DDL_DATABASE_GENERATION) })
+	@GeminiPersistenceContext(unitName = "jFolder", properties = {
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_USER, value = "sa"),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_PASSWORD, value = ""),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_DRIVER, value = "org.h2.Driver"),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_URL, value = JPAConstants.H2_URL_EMBEDDED),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.DDL_GENERATION, value = PersistenceUnitProperties.DROP_AND_CREATE),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.DDL_GENERATION_MODE, value = PersistenceUnitProperties.DDL_DATABASE_GENERATION),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING, value = "false"),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING_INTERNAL, value = "false"),
+			@GeminiPersistenceProperty(name = PersistenceUnitProperties.LOGGING_LEVEL, value = "FINE")
+
+	})
 	private EntityManager em;
 
 	public Verzeichnis create(Verzeichnis verzeichnis) {
 		System.out.println(verzeichnis);
-		if (em != null) {
-			em.persist(verzeichnis);
-		} else {
-			System.out.println("No EntityManager found");
-			EntityManagerFactory emf = Activator.lookupEMF("Accounts");
-			if (emf != null) {
-				EntityManager em = emf.createEntityManager();
-				em.persist(verzeichnis);
-			} else {
-				System.out.println("No EntityManagerFactory found");
+		EntityManager entityManager = em;
+		boolean isSelfCreated = false;
+
+		try {
+			if (entityManager == null) {
+				System.out.println("No DI EntityManager found");
+				entityManager = Activator.getEntityManager();
+				isSelfCreated = true;
 			}
-		
+
+			if (entityManager != null) {
+				entityManager.persist(verzeichnis);
+			} else {
+				System.out.println("No EntityManager from Activator found");
+			}
+
+		} finally {
+			if (isSelfCreated && entityManager != null) {
+				entityManager.close();
+			}
 		}
-		
 		return verzeichnis;
 	}
+
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+
+	// public static EntityManagerFactory lookupEMF(String unitName) {
+	// BundleContext context = Activator.getContext();
+	// ServiceReference refs[] = null;
+	// try {
+	// refs = context.getServiceReferences(
+	// EntityManagerFactory.class.getName(), "(osgi.unit.name="
+	// + unitName + ")");
+	// } catch (InvalidSyntaxException isEx) {
+	// throw new RuntimeException("Filter error", isEx);
+	// }
+	// return (refs == null) ? null : (EntityManagerFactory) context
+	// .getService(refs[0]);
+	// }
+	//
+	// public EntityManagerFactory getEMF(String unitName,
+	// Map<String, String> props) {
+	// ServiceReference[] refs = null;
+	// EntityManagerFactoryBuilder emfb = null;
+	// BundleContext context = Activator.getContext();
+	//
+	// try {
+	// refs = context.getServiceReferences(
+	// EntityManagerFactoryBuilder.class.getName(),
+	// "(osgi.unit.name=" + unitName + ")");
+	// } catch (InvalidSyntaxException isEx) {
+	// throw new RuntimeException("Filter error", isEx);
+	// }
+	// if (refs != null)
+	// emfb = (EntityManagerFactoryBuilder) context.getService(refs[0]);
+	// return (emfb == null) ? null : emfb.createEntityManagerFactory(props);
+	// }
 
 }
--- Plugins/jOrdner.jpa/src/main/java/com/gh/jordner/jpa/osgi/Activator.java
+++ Plugins/jOrdner.jpa/src/main/java/com/gh/jordner/jpa/osgi/Activator.java
@@ -2,6 +2,7 @@ package com.gh.jordner.jpa.osgi;
 
 import java.util.Map;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import org.osgi.framework.Bundle;
@@ -17,13 +18,38 @@ import com.gh.jordner.jpa.JPAConstants;
 
 public class Activator implements BundleActivator, ServiceTrackerCustomizer {
 
-	private static BundleContext context;
-	ServiceTracker emfTracker;
+	private static ServiceTracker emfTracker;
+	private static BundleContext bundleContext;
 
-	// Client client;
+	public void start(BundleContext context) {
+		emfTracker = new ServiceTracker(context,
+				EntityManagerFactory.class.getName(), null);
+		emfTracker.open();
+		System.out.println("Gemini JPA Sample started");
+	}
 
-	static BundleContext getContext() {
-		return context;
+	public void stop(BundleContext context) {
+		this.bundleContext = context;
+		emfTracker.close();
+		emfTracker = null;
+		System.out.println("Gemini JPA Sample stopped");
+	}
+
+	public static EntityManager getEntityManager() {
+		EntityManager entityManager = null;
+		if (emfTracker == null) {
+			return null;
+		}
+		final Object service = emfTracker.getService();
+
+		if (service != null) {
+			EntityManagerFactory emf = (EntityManagerFactory) service;
+			if (emf != null) {
+				entityManager = emf.createEntityManager();
+			}
+		}
+
+		return entityManager;
 	}
 
 	/*
@@ -33,22 +59,22 @@ public class Activator implements BundleActivator, ServiceTrackerCustomizer {
 	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
 	 * )
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-		// client = new Client();
-		System.out.println("Gemini JPA Sample started");
-
-		/*
-		 * We are in the same bundle as the persistence unit so the services
-		 * should be available when we start up (if nothing bad happened) and
-		 * the tracker is really just saving us the lookup, but this is the idea
-		 * of how you would listen for a persistence unit coming from another
-		 * bundle.
-		 */
-		emfTracker = new ServiceTracker(Activator.context,
-				EntityManagerFactory.class.getName(), this);
-		emfTracker.open();
-	}
+	// public void start(BundleContext bundleContext) throws Exception {
+	// Activator.context = bundleContext;
+	// // client = new Client();
+	// System.out.println("Gemini JPA Sample started");
+	//
+	// /*
+	// * We are in the same bundle as the persistence unit so the services
+	// * should be available when we start up (if nothing bad happened) and
+	// * the tracker is really just saving us the lookup, but this is the idea
+	// * of how you would listen for a persistence unit coming from another
+	// * bundle.
+	// */
+	// emfTracker = new ServiceTracker(Activator.context,
+	// EntityManagerFactory.class.getName(), this);
+	// emfTracker.open();
+	// }
 
 	/*
 	 * (non-Javadoc)
@@ -56,74 +82,53 @@ public class Activator implements BundleActivator, ServiceTrackerCustomizer {
 	 * @see
 	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
-		emfTracker.close();
-		// client = null;
-		System.out.println("Gemini JPA Sample stopped");
-	}
+	// public void stop(BundleContext bundleContext) throws Exception {
+	// Activator.context = null;
+	// emfTracker.close();
+	// // client = null;
+	// System.out.println("Gemini JPA Sample stopped");
+	// }
 
-	@Override
-	public Object addingService(ServiceReference reference) {
-		System.out.println("register service reference");
-		Bundle b = reference.getBundle();
-		Object service = b.getBundleContext().getService(reference);
-		String unitName = (String) reference
-				.getProperty(EntityManagerFactoryBuilder.JPA_UNIT_NAME);
-		if (unitName.equals(JPAConstants.JPA_UNIT)) {
-			// TODO
-			// client.run((EntityManagerFactory) service);
-			System.out.println("save called");
-		}
-		return service;
-	}
+	// @Override
+	// public Object addingService(ServiceReference reference) {
+	// System.out.println("register service reference");
+	// Bundle b = reference.getBundle();
+	// Object service = b.getBundleContext().getService(reference);
+	// String unitName = (String) reference
+	// .getProperty(EntityManagerFactoryBuilder.JPA_UNIT_NAME);
+	// if (unitName.equals(JPAConstants.JPA_UNIT)) {
+	// // TODO
+	// // client.run((EntityManagerFactory) service);
+	// System.out.println("save called");
+	// }
+	// return service;
+	// }
 
 	@Override
 	public void modifiedService(ServiceReference reference, Object service) {
-		// TODO Auto-generated method stub
+		System.out.println("modified Service");
 
 	}
 
 	@Override
 	public void removedService(ServiceReference reference, Object service) {
-		// TODO Auto-generated method stub
+		System.out.println("removed Service");
 
 	}
 
-	/**
-	 * User defined methods
-	 */
-
-	public static EntityManagerFactory lookupEMF(String unitName) {
-		BundleContext context = Activator.getContext();
-		ServiceReference refs[] = null;
-		try {
-			refs = context.getServiceReferences((String)null, (String)null);
-			refs = context.getServiceReferences(
-					EntityManagerFactory.class.getName(), "(osgi.unit.name="
-							+ unitName + ")");
-		} catch (InvalidSyntaxException isEx) {
-			throw new RuntimeException("Filter error", isEx);
-		}
-		return (refs == null) ? null : (EntityManagerFactory) context
-				.getService(refs[0]);
+	@Override
+	public Object addingService(ServiceReference reference) {
+		System.out.println("add Service");
+		return null;
 	}
 
-	public EntityManagerFactory getEMF(String unitName,
-			Map<String, String> props) {
-		ServiceReference[] refs = null;
-		EntityManagerFactoryBuilder emfb = null;
-
-		try {
-			refs = context.getServiceReferences(
-					EntityManagerFactoryBuilder.class.getName(),
-					"(osgi.unit.name=" + unitName + ")");
-		} catch (InvalidSyntaxException isEx) {
-			throw new RuntimeException("Filter error", isEx);
-		}
-		if (refs != null)
-			emfb = (EntityManagerFactoryBuilder) context.getService(refs[0]);
-		return (emfb == null) ? null : emfb.createEntityManagerFactory(props);
+	public BundleContext getBundleContext() {
+		return bundleContext;
 	}
+
+	// public void bindEntityManagerFactoryBuilder(EntityManagerFactoryBuilder
+	// emfBuilder, Map properties){
+	//
+	// }
 
 }
