ce1f7e57af2bb317a00d6fe142557f76b6efe4ef
Thomas Schubert <funthomas424242@gmail.com>
2013-08-12 20:18:04 +0200

    usage of interfaces of jOrdner.api project are deprecated
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- Plugins/jOrdner.api/src/com/gh/jordner/api/Verzeichnis.java
+++ Plugins/jOrdner.api/src/com/gh/jordner/api/Verzeichnis.java
@@ -1,14 +1,23 @@
 package com.gh.jordner.api;
 
+/**
+ * 
+ * @author SchubertT006
+ * @deprecated Please use only VerzeichnisImpl of the jOrdner.jpa project
+ */
+@Deprecated
 public interface Verzeichnis {
 
 	public Long getId();
-	public void setId(Long id);
-	
-	public String getName();
-	public void setName(String name) ;
 
-	public String getParentPathURI() ;
-	public void setParentPathURI(String parentPathURI) ;
-	
+	public void setId(Long id);
+
+	public String getName();
+
+	public void setName(String name);
+
+	public String getParentPathURI();
+
+	public void setParentPathURI(String parentPathURI);
+
 }
