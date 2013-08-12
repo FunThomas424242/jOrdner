8e4db0e3f566c5dd366a0f939d1698c841418917
Thomas Schubert <funthomas424242@gmail.com>
2013-08-12 20:55:49 +0200

    project specific exceptions added
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

---
+++ Plugins/jOrdner.api/src/com/gh/jordner/exceptions/CheckedException.java
@@ -0,0 +1,32 @@
+package com.gh.jordner.exceptions;
+
+@SuppressWarnings("serial")
+public class CheckedException extends Exception {
+
+	public CheckedException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CheckedException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CheckedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CheckedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CheckedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Plugins/jOrdner.api/src/com/gh/jordner/exceptions/DataAccessException.java
@@ -0,0 +1,32 @@
+package com.gh.jordner.exceptions;
+
+@SuppressWarnings("serial")
+public class DataAccessException extends CheckedException {
+
+	public DataAccessException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataAccessException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
