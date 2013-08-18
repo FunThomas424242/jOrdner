60dc6673541dc52c49dd42e3bbd64c5212b2f127
Thomas Schubert <funthomas424242@gmail.com>
2013-08-18 19:09:15 +0200

    project renamed
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

---
+++ Plugins/jOrdner.test.natspec/jOrdner.stories/src-endusertests/com/gh/jordner/stories/BasisOperationen.java
@@ -0,0 +1,37 @@
+package com.gh.jordner.stories;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gh.jordner.stories.TestSupport;
+
+/**
+ * JUnit test case generated from the file #className#.natspec. 
+ *
+ * Never modify this file. It will be overwritten by any changes
+ * in #className#.natspec. 
+ */
+public class BasisOperationen {
+
+	private TestSupport testSupport = null;
+
+	@Test
+	public void executeScript() throws Exception {
+		// The code in this method is generated from: /jOrdner.test.natspec/src-endusertests/com/gh/jordner/stories/BasisOperationen.natspec
+		// Never change this method or any contents of this file, all local changes will be overwritten.
+		
+		
+	}
+	
+	@Before
+	public void setUp() {
+		testSupport = new TestSupport();
+	}
+	
+	@After 
+	public void shutdown() {
+		if (testSupport != null)  
+			testSupport = null;
+	}
+}
