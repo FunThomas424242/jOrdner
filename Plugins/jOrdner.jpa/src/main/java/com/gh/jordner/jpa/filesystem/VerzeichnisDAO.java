3acd1773d9283bf06e85e62b61773fb95994e3e9
FunThomas424242 <funthomas424242@gmail.com>
2014-04-14 22:56:20 +0200

    JPA Injection added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- Plugins/jOrdner.jpa/src/main/java/com/gh/jordner/jpa/filesystem/VerzeichnisDAO.java
+++ Plugins/jOrdner.jpa/src/main/java/com/gh/jordner/jpa/filesystem/VerzeichnisDAO.java
@@ -12,77 +12,77 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import jpa.preferences.JPADefaults;
+import jpa.preferences.PreferenceConstants;
+
 import org.eclipse.e4.core.di.annotations.Creatable;
+import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.gemini.ext.di.GeminiPersistenceContext;
+import org.eclipse.gemini.ext.di.GeminiPersistenceProperty;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 @Creatable
 public class VerzeichnisDAO {
 
-	@Inject
-	@GeminiPersistenceContext(unitName = JPAConstants.JPA_UNIT
-	// , properties = {
-	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_USER,
-	// value = "sa"),
-	// @GeminiPersistenceProperty(name =
-	// PersistenceUnitProperties.JDBC_PASSWORD, value = ""),
-	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_DRIVER,
-	// value = "org.h2.Driver"),
-	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_URL,
-	// value = JPAConstants.H2_URL_SERVER),
-	// @GeminiPersistenceProperty(name =
-	// PersistenceUnitProperties.LOGGING_LEVEL, value = "FINE"),
-	// @GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING,
-	// value = "false"),
-	// @GeminiPersistenceProperty(name =
-	// PersistenceUnitProperties.WEAVING_INTERNAL, value = "false"),
-	// @GeminiPersistenceProperty(name =
-	// PersistenceUnitProperties.DDL_GENERATION, value =
-	// PersistenceUnitProperties.DROP_AND_CREATE),
-	// @GeminiPersistenceProperty(name =
-	// PersistenceUnitProperties.DDL_GENERATION_MODE, value =
-	// PersistenceUnitProperties.DDL_DATABASE_GENERATION)
-	// }
-	)
-	private EntityManager em;
+    @Inject
+    @GeminiPersistenceContext(unitName = JPADefaults.JPA_UNIT, properties = {
+            @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_USER, valuePref = @Preference(PreferenceConstants.P_JDBC_USERNAME)),
+            @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_PASSWORD, valuePref = @Preference(PreferenceConstants.P_JDBC_PASSWORD)),
+            @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_DRIVER, valuePref = @Preference(PreferenceConstants.P_JDBC_DRIVER)),
+            @GeminiPersistenceProperty(name = PersistenceUnitProperties.JDBC_URL, valuePref = @Preference(PreferenceConstants.P_JDBC_URL)),
+            @GeminiPersistenceProperty(name = PersistenceUnitProperties.LOGGING_LEVEL, value = "FINE")
+    // @GeminiPersistenceProperty(name = PersistenceUnitProperties.WEAVING,
+    // value = "false"),
+    // @GeminiPersistenceProperty(name =
+    // PersistenceUnitProperties.WEAVING_INTERNAL, value = "false"),
+    // @GeminiPersistenceProperty(name =
+    // PersistenceUnitProperties.DDL_GENERATION, value =
+    // PersistenceUnitProperties.DROP_AND_CREATE),
+    // @GeminiPersistenceProperty(name =
+    // PersistenceUnitProperties.DDL_GENERATION_MODE, value =
+    // PersistenceUnitProperties.DDL_DATABASE_GENERATION)
+    })
+    private EntityManager em;
 
-	public void insert(Verzeichnis dataObj) throws SQLException {
-		checkConnection();
-		EntityTransaction trx = em.getTransaction();
-		trx.begin();
-		em.persist(dataObj);
-		trx.commit();
-	}
+    public void insert(Verzeichnis dataObj) throws SQLException {
+        checkConnection();
+        EntityTransaction trx = em.getTransaction();
+        trx.begin();
+        em.persist(dataObj);
+        trx.commit();
+    }
 
-	public List<Verzeichnis> listAllVerzeichnisse() {
-		// HINT:
-		// http://www.adam-bien.com/roller/abien/entry/selecting_all_jpa_entities_as
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<Verzeichnis> cq = cb.createQuery(Verzeichnis.class);
-		Root<Verzeichnis> rootEntry = cq.from(Verzeichnis.class);
-		CriteriaQuery<Verzeichnis> all = cq.select(rootEntry);
-		TypedQuery<Verzeichnis> allQuery = em.createQuery(all);
-		return allQuery.getResultList();
-	}
+    public List<Verzeichnis> listAllVerzeichnisse() {
+        // HINT:
+        // http://www.adam-bien.com/roller/abien/entry/selecting_all_jpa_entities_as
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Verzeichnis> cq = cb.createQuery(Verzeichnis.class);
+        Root<Verzeichnis> rootEntry = cq.from(Verzeichnis.class);
+        CriteriaQuery<Verzeichnis> all = cq.select(rootEntry);
+        TypedQuery<Verzeichnis> allQuery = em.createQuery(all);
+        return allQuery.getResultList();
+    }
 
-	public void remove(final Verzeichnis verzeichnis) throws SQLException {
-		checkConnection();
-		EntityTransaction trx = em.getTransaction();
-		trx.begin();
-		em.remove(verzeichnis);
-		trx.commit();
-	}
+    public void remove(final Verzeichnis verzeichnis) throws SQLException {
+        checkConnection();
+        EntityTransaction trx = em.getTransaction();
+        trx.begin();
+        em.remove(verzeichnis);
+        trx.commit();
+    }
 
-	@PreDestroy
-	public void destroy() {
-		if (em != null && em.isOpen()) {
-			em.close();
-		}
-	}
+    @PreDestroy
+    public void destroy() {
+        if (em != null && em.isOpen()) {
+            em.close();
+        }
+    }
 
-	private void checkConnection() throws SQLException {
-		if (em == null) {
-			throw new SQLException(
-					"EntityManager is null. Not connected to database!");
-		}
-	}
+    private void checkConnection() throws SQLException {
+        if (em == null) {
+            throw new SQLException(
+                    "EntityManager is null. Not connected to database!");
+        }
+        System.out.println(em.getProperties().toString());
+    }
 }
