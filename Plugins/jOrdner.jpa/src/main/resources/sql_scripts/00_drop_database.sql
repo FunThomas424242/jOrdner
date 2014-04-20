fdbc3f09e110ffc4b92304649f07414009d71f6e
FunThomas424242 <funthomas424242@gmail.com>
2014-04-20 16:32:14 +0200

    sql scripts added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

---
+++ Plugins/jOrdner.jpa/src/main/resources/sql_scripts/00_drop_database.sql
@@ -0,0 +1,11 @@
+--
+-- drop the database
+-- 
+-- attation - you will be lose all datas
+--
+
+--
+-- table verzeichnis
+--
+drop table verzeichnis;
+
---
+++ Plugins/jOrdner.jpa/src/main/resources/sql_scripts/01_create_database.sql
@@ -0,0 +1,13 @@
+--
+-- create the database from scratch
+--
+
+--
+-- table verzeichnis
+--
+create table verzeichnis(
+    id int primary key
+    ,name varchar
+    ,parentpathuri varchar
+);
+
